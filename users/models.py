38099c4ebee118adaacec865cf4a94da386899b2
M@teo22 <djokodev75@gmail.com>
2024-12-02 01:57:24 +0100

    initial commit

---
+++ users/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('L\'adresse email est obligatoire')
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError('Superuser doit avoir is_staff=True.')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('Superuser doit avoir is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+    
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+from django.shortcuts import render
+
+
+def Home(request):
+    return render(request, 'home.html')
+
+class UserRegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+
+class UserLogoutView(LogoutView):
+    template_name = 'users/logout.html'
