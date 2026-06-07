c8cd630c38a3379089e80ae4132653a8dac83817
Djoko Christian <161902861+djokodev@users.noreply.github.com>
2026-06-07 14:56:14 +0100

    fix allowed hosts for vercel deploiment

--- config/settings.py
+++ config/settings.py
@@ -30,10 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False")
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
